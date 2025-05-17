5233c9358c163906347928573e75c6f320b1c001
Sankalpa Dutta <sankalpadutta04@gmail.com>
2025-05-17 07:43:57 +0000

    add more features regarding user login and saving  snippets in database

---
+++ src/contexts/auth-provider.tsx
@@ -0,0 +1,5 @@
+
+"use client";
+
+import type { User } from "@/types";
+import { useRouter }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+
 export interface Snippet {
   id: string;
   title: string;
@@ -7,3 +8,5 @@ export interface Snippet {
   createdAt: Date;
   language?: string; // Optional: for syntax highlighting or filtering in future
 }
+
+export interface
